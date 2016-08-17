cf76433a0e875c0219feaaa502f9eef4adc34594
lixi <mailtolixi@gmail.com>
2016-08-17 22:01:30 +0800

    update

--- SuuWeb.js
+++ SuuWeb.js
@@ -2,7 +2,7 @@
 // document.write(
 //     "<style type=\"text/css\">" +
 //     "   * {margin:0;padding:0}" +
-//     "   body,html,canvas {width:100%;height: 100%;overflow-y: hidden;}" +
+//     "   body,html,canvas {width:100%;height: 100%;overflow-y: hidden;}" + 
 //     "</style>");
 document.write(
     "<style type=\"text/css\">" +
